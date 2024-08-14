5e89bd9edb6285ff52da5468ad103be311cdd7a9
Gjorgjieva Forka <149876641+GjorgjievaF@users.noreply.github.com>
2024-08-15 00:12:14 +0200

    Add files via upload

---
+++ Class02_homework/Class02_homework/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Class02_homework.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+
+        [HttpGet("allUsers")] //http://localhost[port]/api/Users/allUsers
+        public ActionResult<List<string>> GetAllUsers()
+        {
+
+            return Ok(StaticDb.Names);
+
+
+        }
+
+        [HttpGet("{index}")]  //http://localhost[port]/api/Users/1
+        public ActionResult<string> GetUserByIndex(int index)
+        {
+            try
+            {
+                if(index < 0)
+                {
+                    return BadRequest($"There is no user with index {index}");
+                }
+
+                if (index >= StaticDb.Names.Count )
+                {
+                    return NotFound($"There is no user with index {index}");
+                }
+
+                return StaticDb.Names[index];   
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,ex.Message);
+
+            }
+        }
+
+
+    }
+}
---
+++ Class02_homework/Class02_homework/StaticDb.cs
@@ -0,0 +1,10 @@
+﻿namespace Class02_homework
+{
+    public static class StaticDb
+    {
+        public static List<string> Names = new List<string>()
+        {
+            "Bob","Jill","Mike","John","Mark","Ana"
+        };
+    }
+}
